24774b34767738d0f1109697dae293e430a17796
farisabqari01 <faris055abqari@gmail.com>
2025-12-09 15:50:31 +0800

    Pertemuan 4

---
+++ belajar-php.php
@@ -0,0 +1,37 @@
+<?php
+$a = 1
+$b ="1";
+$c = $a+$b;
+$d = $a.$b;
+$e = $a*$b;
+echo "hello world!"
+echo "<br>":
+echo $c;
+echo "<br>"
+echo $d;
+echo "<br>"
+echo $e;
+$f= ["Hijau","Kuning","Merah"];
+// Syarat cetaknya = $f=[]
+
+echo"
+<div class='container'>
+    <h1>ini kode html </h1>
+</div>";
+
+if($d>12){
+?>
+<div class+="container">
+    <h1>inti code html</h1>
+</div>
+<?php>
+}
+
+echo "<br>";
+echo $e;
+echo "<br>";
+echo $f[0];
+echo "<br>";
+var_dump  ($f);
+
+?>
---
+++ belajar_getpost.php
@@ -0,0 +1,5 @@
+<form action="" method="get">
+    <input type="text" name="id" id="">
+    <button type="submit">Kirim</button>
+
+</form>
